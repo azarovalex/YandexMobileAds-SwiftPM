00e4c3b5adc9f762c597051d49a663e090fa0aa4
Alexander Azarov <azarovalex@yandex-team.ru>
2020-10-09 10:25:02 +0300

    add dependecies

--- Package.swift
+++ Package.swift
@@ -8,9 +8,33 @@ let package = Package(
     products: [
         .library(
             name: "YandexMobileAds",
-            targets: ["YandexMobileAds"]),
+            targets: ["YandexMobileAdsWrapper"]),
     ],
     targets: [
+        .target(
+            name: "YandexMobileAdsWrapper",
+            dependencies: [
+                .target(name: "YandexMobileAds", condition: .when(platforms: .some([.iOS]))),
+            ],
+            path: "YandexMobileAdsWrapper",
+            linkerSettings: [
+                .linkedLibrary("sqlite3"),
+                .linkedLibrary("c++"),
+                .linkedLibrary("z"),
+                .linkedLibrary("xml2"),
+                .linkedFramework("StoreKit"),
+                .linkedFramework("Security"),
+                .linkedFramework("SafariServices"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("CoreLocation"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("AdSupport"),
+                .linkedFramework("WebKit")
+            ]
+        ),
         .binaryTarget(name: "YandexMobileAds", path: "./YandexMobileAds.xcframework")
     ]
 )
